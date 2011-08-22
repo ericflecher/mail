5e23c553f57066958839e9d947ede26596fa2484
ruby <ruby@Eric-Flechers-MacBook-Air.local>
2011-08-21 22:15:35 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mail::Application.config.secret_token = '70e4f6a8d65560db41c4315f3472c45ce049f2a76d6622a82de2a33b471e5b337f8155036e052bf9048ef72dbe2c67f329f63bd7dd24131ad2db117b5e21c432'
